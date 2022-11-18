7c28008e6016c0d148a60be77dff712819822088
Josh Martin <josh.martin@cmtp.com.au>
2022-11-18 15:09:23 +1100

    js and css files updated and map is functioning

--- Leaflet-Part-1/static/js/logic.js
+++ Leaflet-Part-1/static/js/logic.js
@@ -1,3 +1,78 @@
-d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_month.geojson").then(function(data) {
-    console.log(data);
+let url = ("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson");
+
+
+d3.json(url).then(function(data) {
+    // colorLegend(data.bbox);
+    // createFeatures(data.features);
+    createMap(data.features);
   });
+
+
+function createMap(earthquakes) {
+
+
+// Create a map object.
+let myMap = L.map("map-id", {
+    center: [-22.839076, 91.593792],
+    zoom: 3
+  });
+  
+  // Add a tile layer.
+  L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+  }).addTo(myMap);
+
+    // Loop through the cities array, and create one marker for each city object.
+    for (let i = 0; i < earthquakes.length; i++) {
+
+        let location = [earthquakes[i].geometry.coordinates[1], earthquakes[i].geometry.coordinates[0]]
+  
+        // Add circles to the map.
+        L.circle(location, {
+          fillOpacity: 0.75,
+          weight: 2,
+          color: "white",
+          fillColor: getColor(earthquakes[i].geometry.coordinates[2]),
+          // Adjust the radius.
+            //   radius: Math.sqrt(countries[i].gdp_pc) * 500
+            radius: earthquakes[i].properties.mag * 50000
+        }).bindPopup(`<h2><strong>Earthquake event</strong></h2><hr>
+        <p><strong>Location:</strong> ${earthquakes[i].properties.place}</p>
+        <p><strong>Magnitude:</strong>${earthquakes[i].properties.mag} </p>
+        <p><strong>Depth:</strong>${earthquakes[i].geometry.coordinates[2]} </p>
+        <p><strong>Time of Event:</strong> ${new Date(earthquakes[i].properties.time)}</p>`).addTo(myMap);
+    }
+                 
+                      
+
+
+        var legend = L.control({position: 'bottomright'});
+
+        legend.onAdd = function (map) {
+        
+            var div = L.DomUtil.create('div', 'info legend'),
+                grades = [-10, 10, 30, 50, 70, 90],
+                labels = [];
+        
+            // loop through our density intervals and generate a label with a colored square for each interval
+            for (var i = 0; i < grades.length; i++) {
+                div.innerHTML +=
+                    '<i style="background:' + getColor(grades[i] + 1) + '"></i> ' +
+                    grades[i] + (grades[i + 1] ? '&ndash;' + grades[i + 1] + '<br>' : '+');
+            }
+        
+            return div;
+        };
+        
+        legend.addTo(myMap);
+
+        function getColor(d) {
+            return d > 90 ? '#006837' :
+                   d > 70  ? '#31a354' :
+                   d > 50  ? '#78c679' :
+                   d > 30  ? '#addd8e' :
+                   d > 10  ? '#d9f0a3' :
+                   d > -10   ? '#ffffcc':
+                   '#FFEDA0';
+        };
+  };
